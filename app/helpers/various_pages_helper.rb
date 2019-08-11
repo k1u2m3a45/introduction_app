b95e3c7418e47ab1f48fd1c5a593f01778d2788a
Kana <K1u2m3a45@gmail.com>
2019-08-11 11:11:35 +0900

    1st layout

---
+++ app/helpers/various_pages_helper.rb
@@ -0,0 +1,2 @@
+module VariousPagesHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
 
-  root 'application#hello'
+  #root 'application#hello'
+
+  root 'various_pages#home'
+
+  get '/profile',to:'various_pages#profile'
+
+  get '/skill',to:'various_pages#skill'
+
+  get '/output',to:'various_pages#output'
+
+  get '/contact',to:'various_pages#contact'
+
+
 
 end
